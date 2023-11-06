0c935c35a832d51dc0c4e8ea01d2b4def9f0cc5c
Matija Ojo <matija.ojo@gmail.com>
2023-11-06 10:48:17 +0100

    basic boid simulation

--- boid.py
+++ boid.py
@@ -1,17 +1,17 @@
 import pygame as pg
 import random
+import constants
 
 
 class Boid:
     debug = False
     can_wrap = False
-    min_speed = 0.01
-    max_speed = 0.2
+    min_speed = 100
+    max_speed = 500
     max_force = 1
     max_turn = 5
-    perception = 1000
-    separation_distance = 15
-    edge_distance_pct = 5
+    perception_radius = 50
+    separation_distance = 25
 
     def __init__(self):
         max_x, max_y = 800, 600
@@ -22,7 +22,12 @@ class Boid:
             random.uniform(-10, 10),
             random.uniform(-10, 10),
         )
-        start_velocity = Boid.max_speed * random_velocity / random_velocity.length()
+        start_velocity = (
+            (Boid.max_speed + Boid.min_speed)
+            / 2
+            * random_velocity
+            / random_velocity.length()
+        )
 
         self.position = start_position
         self.velocity = start_velocity
@@ -35,27 +40,26 @@ class Boid:
         s = self.separation(neighbors)
         c = self.cohesion(neighbors)
         a = self.alignment(neighbors)
-        direction = s + c + a
+        b = self.bound_position()
+        direction = s + c + a + b
 
-        if direction.length_squared() > 0:
-            self.acceleration = direction
-        # TODO: Enforce turn limits
+        self.velocity += direction
 
-        self.velocity += self.acceleration
+        # if direction.length_squared() > 0:
+        #     self.acceleration = direction
+        # # TODO: Enforce turn limits
+
+        # self.velocity += self.acceleration
+
+        # Speed limit
         speed = self.velocity.length()
         if speed < self.min_speed:
             self.velocity.scale_to_length(self.min_speed)
         if speed > self.max_speed:
             self.velocity.scale_to_length(self.max_speed)
 
-        # lol...
-        if not (0 <= self.position.x <= 800):
-            self.velocity.x *= -1
-        if not (0 <= self.position.y <= 600):
-            self.velocity.y *= -1
-
-        # self.position += self.velocity * dt
-        self.position += self.velocity
+        self.position += self.velocity * dt
+        # self.position += self.velocity
 
     def draw(self, screen):
         pg.draw.circle(screen, (0, 0, 255), self.position, 5)
@@ -65,15 +69,10 @@ class Boid:
         for boid in boids:
             if boid is not self:
                 dist_sq = self.position.distance_squared_to(boid.position)
-                if dist_sq < self.perception**2:
+                if dist_sq < self.perception_radius**2:
                     neighbors.append(boid)
 
         return neighbors
-
-    def clamp_force(self, force: pg.Vector2):
-        if force.magnitude() > self.max_force:
-            force.scale_to_length(self.max_force)
-        return force
 
     def separation(self, neighbors):
         direction = pg.Vector2(0, 0)
@@ -83,17 +82,16 @@ class Boid:
             ).length_squared() < Boid.separation_distance**2:
                 direction -= boid.position - self.position
 
-        return self.clamp_force(direction)
+        return direction
 
     def cohesion(self, neighbors):
         if len(neighbors) == 0:
             return pg.Vector2(0, 0)
         direction = pg.Vector2()
         for boid in neighbors:
-            direction += boid.position
+            direction += boid.position - self.position
         direction /= len(neighbors)
-        direction = self.clamp_force(direction)
-        return direction / 100  # Move 1% to the centre
+        return direction / 100  # Move 1% to the center
 
     def alignment(self, neighbors):
         if len(neighbors) == 0:
@@ -103,5 +101,19 @@ class Boid:
             direction += boid.velocity
 
         direction /= len(neighbors)
-        direction = self.clamp_force(direction - self.velocity)
+        direction -= self.velocity
         return direction / 8
+
+    def bound_position(self):
+        direction = pg.Vector2(0, 0)
+        if self.position.x <= constants.DISTANCE_TO_BOUNDS:
+            direction.x = 10
+        elif self.position.x >= constants.WIDTH - constants.DISTANCE_TO_BOUNDS:
+            direction.x = -10
+
+        if self.position.y <= constants.DISTANCE_TO_BOUNDS:
+            direction.y = 10
+        elif self.position.y >= constants.HEIGHT - constants.DISTANCE_TO_BOUNDS:
+            direction.y = -10
+
+        return direction
--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import pygame as pg
-import boid
+import boid as boid_class
+import constants
 
-WIDTH, HEIGHT = 800, 600
-fps = 60
-dt = 1 / fps
+
+def init_boids(n):
+    return [boid_class.Boid() for _ in range(n)]
+
 
 pg.init()
-
-# Set up the drawing window
-screen = pg.display.set_mode([WIDTH, HEIGHT])
+screen = pg.display.set_mode([constants.WIDTH, constants.HEIGHT])
 clock = pg.time.Clock()
 
-boids = [boid.Boid() for _ in range(10)]
-
+fps = 60
+dt = 1 / fps
+boids = init_boids(constants.N_BOIDS)
 running = True
+
+
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_r:
+                boids = init_boids(constants.N_BOIDS)
+            elif event.key == pg.K_ESCAPE:
+                running = False
 
     # Fill (background)
     screen.fill((0, 0, 0))
